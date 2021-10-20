65b00cda63676b3f2f93e3ded18b3282720d6124
AServantDev <arnaud.github@gmail.com>
2021-10-20 20:23:46 +0200

    changes requested

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -8,14 +8,11 @@ import { usePlayer } from './context/PlayerContext';
 
 export const GamePage = (): JSX.Element => {
   const location = useLocation().pathname.split("/");
-  console.log(location);
   const players = usePlayer();
   const displayPlayers = () => {
     return players
       .filter((player) => player.games.includes(location[2]))
       .map((filteredPlayer) => {
-        console.log(filteredPlayer.games);
-
         return (
           <HStack>
             <Card
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FiChevronDown } from 'react-icons/fi';
 
-import { Box, Button, HStack, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { Box, Button, HStack, Menu, MenuButton, MenuList } from '@chakra-ui/react';
 
 import { NavBarItem } from './NavBarItem';
 
